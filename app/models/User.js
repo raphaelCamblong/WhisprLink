5ed90b436aace13261ec8c6eef3f87b9d71a3704
raphael <camblong.raphael@epitech.eu>
2023-06-08 23:01:48 +0200

    [ADD] User model

---
+++ app/models/User.js
@@ -0,0 +1,9 @@
+class User {
+  constructor(name, profilepictureURL) {
+    this.name = "name";
+    this.profilepictureURL = "profilepictureURL";
+    this.uid = "uid";
+  }
+}
+
+export default User;
