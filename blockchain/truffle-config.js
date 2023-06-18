26b4874d1a672767ac7887d37991b96e76fd691b
raphael <camblong.raphael@epitech.eu>
2023-06-18 18:43:32 +0200

    [ADD] environment variable

--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -25,25 +25,14 @@
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache, geth, or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
     development: {
       host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+    },
+    developmentDocker: {
+      host: "backend",
       port: 8545,
       network_id: "*",
     },
@@ -55,15 +44,13 @@ module.exports = {
     // }
   },
 
-  // Set default mocha options here, use special reporters etc.
   mocha: {
     // timeout: 100000
   },
 
-  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.13", // Fetch exact version from solc-bin
+      version: "0.8.13",
     },
   },
 };
